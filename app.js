932de7ba6098cbd230f8668e08b429f4796bde71
Gabriel Boubonis <boubonisgabriel@gmail.com>
2024-11-23 12:41:10 -0300

    Primer progreso

---
+++ app.js
@@ -0,0 +1,34 @@
+let campeon_1 = {
+    nombre: "JINX",
+    tipo: "ADC",
+    danio: 500,
+    vida: 350,
+}
+let campeon_2 = {
+    nombre: "VI",
+    tipo: "JUNGLA",
+    danio: 700,
+    vida: 500,
+}
+let campeon_3 = {
+    nombre: "LUX",
+    tipo: "SOPORTE",
+    danio: 300,
+    vida: 600,
+}
+let campeon_4 = {
+    nombre: "VARUS",
+    tipo: "ADC",
+    danio: 450,
+    vida: 500,
+}
+
+function ingreso_campeones() {
+    
+}
+
+function batalla(vida, danio, vida2, danio2) {
+    return vida-danio2, vida2-danio;
+}
+
+console.log("LOL")
