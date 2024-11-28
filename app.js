d60e491b195067ed7234847ff2aa6e3a1b83c1ea
Gabriel Boubonis <boubonisgabriel@gmail.com>
2024-11-28 08:51:36 -0300

    Segundo progreso

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-let campeon_1 = {
-    nombre: "JINX",
-    tipo: "ADC",
-    danio: 500,
-    vida: 350,
-}
-let campeon_2 = {
-    nombre: "VI",
-    tipo: "JUNGLA",
-    danio: 700,
-    vida: 500,
-}
-let campeon_3 = {
-    nombre: "LUX",
-    tipo: "SOPORTE",
-    danio: 300,
-    vida: 600,
-}
-let campeon_4 = {
-    nombre: "VARUS",
-    tipo: "ADC",
-    danio: 450,
-    vida: 500,
-}
 
 function ingreso_campeones() {
     
